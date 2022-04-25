8f65f663cb6cb3028eb85ed699742bc31aa59d50
Sergio Hinojosa <sergio.hinojosa1@gmail.com>
2022-04-24 19:59:14 -0500

    Create process class, classify processes by their type.

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const form = document.querySelector("form");
 const tbody = document.querySelector("tbody");
+const burstTime = document.getElementById("burst-time");
+const processType = document.getElementById("process-type");
 
 // queues
 const systemQueue = [];
 const interactiveQueue = [];
 const batchQueue = [];
+
+class Process {
+  constructor(burstTime) {
+    this.burstTime = burstTime;
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // add process to appropiate queue
+  if (processType.value === "Proceso de Sistema")
+    systemQueue.push(new Process(burstTime.value));
+  else if (processType.value === "Proceso Interactivo")
+    interactiveQueue.push(new Process(burstTime.value));
+  else if (processType.value === "Proceso por Lotes")
+    batchQueue.push(new Process(burstTime.value));
+});
